65788b8d2439c40234dca82f90029d86b4588039
Andrea Trajkovic <trajkovicandrea@gmail.com>
2022-09-07 19:53:32 +0200

    Update

--- github.js
+++ github.js
@@ -1,14 +1,14 @@
 class GitHub {
   constructor() {
-    this.client_id = 'd2390423940asdds1293109d';
-    this.client_secret = '0923840932sadkj5430953409509324bb90b345f324';
+    this.client_id = 'd9308aacf8b204d361fd';
+    this.client_secret = '84969aeef73956f4ec9e8716d1840532802bb81b';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`
     https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
-    const profileData = await profileResponse.json();
+    const profile = await profileResponse.json();
 
     return {
       profile,
